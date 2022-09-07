0f45916f754151fe1cc17446aff0f20ca5ed3b12
Oleg <oleg.beliy@e-u.edu.ua>
2022-09-07 15:12:10 +0300

    Save

---
+++ Main.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+                                                    /*№1*\
+       /* Scanner first = new Scanner(System.in);
+        System.out.println("Введіть перше число від 1 до 1000");
+        int num1 = first.nextInt();
+        Scanner second = new Scanner(System.in);
+        System.out.println("Введіть друге число від 1 до 1000");
+        int num2 = second.nextInt();
+        int res;
+        res = num1 * num1 + num2 * num2 ;
+        System.out.println("Гіпотенуза " + " = " +Math.sqrt(res));
+        */
+
+
+
+                                                    /*№2*\
+        /*
+        Scanner first = new Scanner(System.in);
+        System.out.println("Введіть  число від 1 до 10000");
+        int num1 = first.nextInt();
+        int n = 1;
+        System.out.println("The next number for the number"+ " " +(num1)+ " is " +(num1+n)+ ".");
+        System.out.println("The previous number for the number"+ " " +(num1)+ " is " +(num1-n)+ ".");
+        */
+
+
+
+                                                    /*№3(1)*\
+        /*
+        Scanner first = new Scanner(System.in);
+        System.out.println("Введіть кількість яблук");
+        int num1 = first.nextInt();
+        Scanner second = new Scanner(System.in);
+        System.out.println("Введіть кількість учнів");
+        int num2 = second.nextInt();
+        int res1 ;
+        int res2 ;
+        int res3 ;
+        res1 = num1 % num2 ;
+        res2 = num1 - res1;
+        res3 = res2 / num2;
+        System.out.println("Кількість яблук у учня " + " = " +(res3));
+         */
+
+
+
+
+                                                    /*№3(2)*\
+        /*
+        Scanner first = new Scanner(System.in);
+        System.out.println("Введіть кількість яблук");
+        int num1 = first.nextInt();
+        Scanner second = new Scanner(System.in);
+        System.out.println("Введіть кількість учнів");
+        int num2 = second.nextInt();
+        int res;
+        res = num1 % num2 ;
+        System.out.println("Кількість яблук у кошику " + " = " +(res));
+         */
+    }
+}
